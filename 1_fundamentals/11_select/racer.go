9763479a165a1f4f33248acedd7457e0a19ede86
Mona Lisa <affanwhat@gmail.com>
2026-04-05 17:49:16 +0700

    select (v1)

--- 1_fundamentals/11_select/racer.go
+++ 1_fundamentals/11_select/racer.go
@@ -1,5 +1,23 @@
 package selects
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(a, b string) string {
-	return ""
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	startURL := time.Now()
+	http.Get(url)
+	return time.Since(startURL)
 }
